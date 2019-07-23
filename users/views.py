5a913e2dff7ebe84689493612dcd6ba6c2f0888c
Conor Walsh <conorwalsh0703@gmail.com>
2019-07-23 20:18:06 +0100

    created login functionality

--- users/views.py
+++ users/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to login')
             form.save()
-            return redirect('blog-home')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
