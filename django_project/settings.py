7806c15044626ea3eec5652d9e97df387e3c63e5
Conor Walsh <conorwalsh0703@gmail.com>
2019-07-29 14:45:04 +0100

    added enviromental variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r7d-!f3(g=!*#d@ufv4*jyyxb)aun%5d%_iy@fkwe^n744c^vg'
+# SECRET_KEY = 'r7d-!f3(g=!*#d@ufv4*jyyxb)aun%5d%_iy@fkwe^n744c^vg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cw-django-app.herokuapp.com']
 
 
 # Application definition
